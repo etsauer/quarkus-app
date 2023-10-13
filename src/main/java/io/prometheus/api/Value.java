61d018c14a03834f1e61481efe57c68b274de46e
Eric Sauer <esauer@redhat.com>
2023-10-13 10:42:54 -0400

    Allow for singular values and lists of values

--- src/main/java/io/prometheus/api/Value.java
+++ src/main/java/io/prometheus/api/Value.java
@@ -7,5 +7,5 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 @JsonDeserialize(using = ValueDeserializer.class)
 public record Value (
     Date timestamp,
-    int value
+    Double value
 ) {}
--- src/main/java/io/prometheus/api/ValueDeserializer.java
+++ src/main/java/io/prometheus/api/ValueDeserializer.java
@@ -8,19 +8,16 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.DoubleNode;
-import com.fasterxml.jackson.databind.node.TextNode;
 
 public class ValueDeserializer extends JsonDeserializer<Value> {
 
     @Override
     public Value deserialize(JsonParser p, DeserializationContext cxt) throws IOException, JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
         JsonNode node = p.getCodec().readTree(p);
         double timeStamp = (Double) ((DoubleNode) node.get(0)).doubleValue();
         Date time = new Date((long)timeStamp*1000);
-        int value = Integer.valueOf(node.get(1).asText());
+        Double value = Double.valueOf(node.get(1).asText());
         return new Value(time, value);
     }
     
