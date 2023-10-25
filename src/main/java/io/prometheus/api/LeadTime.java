62079019a529b4e4c8ba1e23c244f7bf6d5f6849
Eric Sauer <null>
2023-10-25 17:05:08 +0000

    Add notnull annotations

--- src/main/java/io/prometheus/api/LeadTime.java
+++ src/main/java/io/prometheus/api/LeadTime.java
@@ -1,6 +1,10 @@
 package io.prometheus.api;
 
+import javax.validation.constraints.NotEmpty;
+
 public record LeadTime (
+    @NotEmpty(message = "{ltfc.lead_time.empty}")
     Double ltfc,
+    @NotEmpty(message = "{ltfc.lead_time_last.empty}")
     Double last
 ) {}
